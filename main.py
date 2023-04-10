870834c31d08b5c92bee6f1bde5434bd755903d3
BabitaPatel1 <22063247-BabitaPatel1@users.noreply.replit.com>
2023-04-10 11:10:47 +0000

    api add

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': "Banglore",
+    'salary': '15,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': "Banglore",
+    'salary': '10,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Software Engineer',
+    'location': "Pune"
+  },
+]
 
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='ABC')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
