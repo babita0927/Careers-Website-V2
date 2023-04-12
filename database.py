34b0b9dab279f047baedd22cb20be22887f177ce
babita0927 <121121436+babita0927@users.noreply.github.com>
2023-04-12 12:47:17 +0000

    add connections

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, text
+import pymssql
+import os
+
+db_connection = os.environ['DB_CONNECTION']
+engine = create_engine(db_connection,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_jobs_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs"))
+
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+
+    return jobs
--- main.py
+++ main.py
@@ -1,36 +1,19 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_db
 
 app = Flask(__name__)
-
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': "Banglore",
-    'salary': '15,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': "Banglore",
-    'salary': '10,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Software Engineer',
-    'location': "Pune"
-  },
-]
 
 
 @app.route('/')
 def home():
-  return render_template('home.html', jobs=JOBS, company_name='ABC')
+  jobs = load_jobs_db()
+  return render_template('home.html', jobs=jobs, company_name='ABC')
 
 
 @app.route('/api/jobs')
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_db()
+  return jsonify(jobs)
 
 
 if __name__ == '__main__':
