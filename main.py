b6cf72ba3cf29d69dde670e40c2830c26afa0577
babita0927 <121121436+babita0927@users.noreply.github.com>
2023-04-12 16:07:02 +0000

    added job page

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_db
+from database import load_jobs_db, load_jobs_from_db
 
 app = Flask(__name__)
 
@@ -7,13 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   jobs = load_jobs_db()
-  return render_template('home.html', jobs=jobs, company_name='ABC')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route('/api/jobs')
 def list_jobs():
   jobs = load_jobs_db()
   return jsonify(jobs)
+
+
+@app.route('/job/<id>')
+def show_job(id):
+  job = load_jobs_from_db(id)
+  if not job:
+    return "Not Found, 404"
+  else:
+    return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
