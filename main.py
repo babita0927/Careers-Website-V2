1f4236ce1d69284d671462eb0da4b97d43e1b497
babita0927 <121121436+babita0927@users.noreply.github.com>
2023-04-14 16:39:31 +0000

    main update

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_db, load_jobs_from_db, add_application
-from forms import ApplicationForm
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'mysecretkey'
-
-csrf = CSRFProtect(app)
 
 
 @app.route('/')
@@ -29,19 +23,6 @@ def show_job(id):
     return "Not Found, 404"
   else:
     return render_template('jobpage.html', job=job)
-
-
-@app.route('/job/<id>/apply', methods=['POST'])
-def apply_to_job(id):
-  job = load_jobs_from_db(id)
-  if not job:
-    return "Not Found, 404"
-  form = ApplicationForm()
-  if form.validate_on_submit():
-    data = form.data
-    add_application(id, data)
-    return render_template('application_submitted.html', apply=data, job=job)
-  return render_template('jobpage.html', job=job, form=form)
 
 
 @app.route('/aboutus')
