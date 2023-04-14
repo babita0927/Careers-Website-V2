dd72555511c20e13e0e68d958564fc324bbafcce
babita0927 <121121436+babita0927@users.noreply.github.com>
2023-04-14 16:28:26 +0000

    route update

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from database import load_jobs_db, load_jobs_from_db, add_application
+from forms import ApplicationForm
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+csrf = CSRFProtect(app)
 
 
 @app.route('/')
@@ -25,12 +31,32 @@ def show_job(id):
     return render_template('jobpage.html', job=job)
 
 
-@app.route('/job/<id>/apply', methods=['post'])
+@app.route('/job/<id>/apply', methods=['POST'])
 def apply_to_job(id):
-  data = request.form
   job = load_jobs_from_db(id)
-  add_application(id, data)
-  return render_template('application_submitted.html', apply=data, job=job)
+  if not job:
+    return "Not Found, 404"
+  form = ApplicationForm()
+  if form.validate_on_submit():
+    data = form.data
+    add_application(id, data)
+    return render_template('application_submitted.html', apply=data, job=job)
+  return render_template('jobpage.html', job=job, form=form)
+
+
+@app.route('/aboutus')
+def about():
+  return render_template('about.html')
+
+
+@app.route('/featured_job')
+def feature():
+  return render_template('featured_job.html')
+
+
+@app.route('/pricing')
+def pricing():
+  return render_template('pricing.html')
 
 
 if __name__ == '__main__':
